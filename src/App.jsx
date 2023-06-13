9242d653aa5ebc0b4be98c7d86329b3d25decb85
parunchxi <parunchxi@gmail.com>
2023-06-13 16:30:13 +0700

    Update search

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,53 @@
 // import React from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Navbar from "./component/Navbar.jsx";
 import Gallery from "./component/Gallery.jsx";
 
 const accessKey = "8bCYPWN5Ffah7eDYFk7VclNa1xCGFgX2qzvqqQWpLdI";
+let prevSearch;
 
 function App() {
   const [searchText, setSearchText] = useState("");
   const [data, setData] = useState([]);
 
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   function handleInputChange(event) {
     setSearchText(event.target.value);
   }
 
+  function checkSearch() {
+    if (prevSearch !== searchText) {
+      prevSearch = searchText;
+      setData([]);
+    }
+  }
+
   async function fetchData() {
     try {
-      const response = await fetch(
-        `https://api.unsplash.com/search/photos?page=1&query=${searchText}&client_id=${accessKey}`
-      );
-      const json = await response.json();
-      json.results.forEach((item) => {
-        setData((prevdata) => {
-          return [...prevdata, item];
+      if (searchText === "") {
+        const response = await fetch(
+          `https://api.unsplash.com/photos/random?count=10&client_id=${accessKey}`
+        );
+        const json = await response.json();
+        json.forEach((item) => {
+          setData((prevdata) => {
+            return [...prevdata, item];
+          });
         });
-      });
+      } else {
+        const response = await fetch(
+          `https://api.unsplash.com/search/photos?page=1&query=${searchText}&client_id=${accessKey}`
+        );
+        const json = await response.json();
+        json.results.forEach((item) => {
+          setData((prevdata) => {
+            return [...prevdata, item];
+          });
+        });
+      }
     } catch (err) {
       console.log(err);
     }
@@ -31,7 +55,7 @@ function App() {
 
   function handleFormSubmit(event) {
     event.preventDefault();
-
+    checkSearch();
     fetchData();
   }
 
--- src/component/Gallery.jsx
+++ src/component/Gallery.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import styles from "./Gallery.module.scss";
 
 function Gallery({ data }) {

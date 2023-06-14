1b25cea3186cbb73bd03c11bde634bcb74940567
parunchxi <parunchxi@gmail.com>
2023-06-14 23:21:23 +0700

    Update style

--- src/component/Gallery.jsx
+++ src/component/Gallery.jsx
@@ -1,15 +1,12 @@
 /* eslint-disable react/prop-types */
 import styles from "./Gallery.module.scss";
+import Photo from "./Photo";
 
 function Gallery({ data }) {
   return (
     <div className={styles.gallery}>
       {data.map((item) => {
-        return (
-          <div key={item.id}>
-            <img src={item.urls.regular} />
-          </div>
-        );
+        return <Photo key={item.id} item={item} />;
       })}
     </div>
   );
--- src/component/Photo.jsx
+++ src/component/Photo.jsx
@@ -1,5 +1,16 @@
-function Photo() {
-  return <div>Photo</div>;
+/* eslint-disable react/prop-types */
+import { RiHeart3Fill } from "react-icons/ri";
+import styles from "./Photo.module.scss";
+
+function Photo({ item }) {
+  return (
+    <div className={styles.photo}>
+      <div className={styles.filter}>
+        <RiHeart3Fill className={styles.love} />
+      </div>
+      <img src={item.urls.regular} />
+    </div>
+  );
 }
 
 export default Photo;
